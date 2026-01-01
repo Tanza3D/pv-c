d37d9522cd6964aa3ad8fcd15c1bbec6c6a36835
Tanza3D <archie@untone.uk>
2026-01-01 21:10:13 +0000

    add previewer

--- src/Eye.cpp
+++ src/Eye.cpp
@@ -3,16 +3,18 @@
 
 
 Eye::Eye()
-        : name(""), 
-          eyeTexture("", "", ColorChannel::BLUE),
-          pupilAreaTexture("", "", ColorChannel::RED),
-          pupilShapeTexture("", "", ColorChannel::GREEN) {}
+    : name(""),
+      eyeTexture("", "", ColorChannel::BLUE),
+      pupilAreaTexture("", "", ColorChannel::RED),
+      pupilShapeTexture("", "", ColorChannel::GREEN) {
+}
 
-Eye::Eye(const std::string& name, const std::string& path)
-        : name(name), 
-          eyeTexture(path, name, ColorChannel::BLUE),
-          pupilAreaTexture(path, name, ColorChannel::RED),
-          pupilShapeTexture(path, name, ColorChannel::GREEN) {}
+Eye::Eye(const std::string &name, const std::string &path)
+    : name(name),
+      eyeTexture(path, name, ColorChannel::BLUE),
+      pupilAreaTexture(path, name, ColorChannel::RED),
+      pupilShapeTexture(path, name, ColorChannel::GREEN) {
+}
 
 Texture Eye::GetEyeTexture() const {
     return eyeTexture;
@@ -26,14 +28,14 @@ Texture Eye::GetPupilShapeTexture() const {
     return pupilShapeTexture;
 }
 
-void Eye::SetColourOverride(std::vector<std::vector<TZColor>> gradient) {
+void Eye::SetColourOverride(std::vector<std::vector<TZColor> > gradient) {
 #ifndef ESP_PLATFORM
     hasOverride = true;
     override = gradient;
 #endif
 }
 
-void Eye::SetPupilColourOverride(std::vector<std::vector<TZColor>> gradient) {
+void Eye::SetPupilColourOverride(std::vector<std::vector<TZColor> > gradient) {
 #ifndef ESP_PLATFORM
     hasPupilOverride = true;
     pupilOverride = gradient;
